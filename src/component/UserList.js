cc1f59220b591e6a90d3e8ceebccca59195212f7
AlvaroHPoveda <alvaropoveda@ingenieros.com>
2022-01-16 16:59:17 -0500

    firt commit

---
+++ src/component/UserList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const UserList = ({users, selectUsers, deleteUsers}) => {
+           
+    return (
+        <div className='card'>
+             {users.map((user) => (
+                <ul className='user-item'key={user.id}>
+                    <div className='card-part01'>
+                        <strong>{user.first_name} {user.last_name}</strong>     
+                        <li>{user.email}</li>
+                        <li>{user.birthday}</li>
+                    </div>
+                    <div className='card-part02'>
+                        <button className='buttonCard' type='button' onClick={()=>deleteUsers(user.id)}><i class="fas fa-trash-alt"></i></button>
+                        <button className='buttonCard' type='button' onClick={()=>selectUsers(user)}><i class="fas fa-pencil-alt"></i></button>
+                    </div>
+                </ul>
+            ))}    
+        </div>
+    );
+};
+
+export default UserList;
